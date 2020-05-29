37fff625d03c60a0492ceb6ffc573deb1cd85562
Michael Harris <mwharris7@gmail.com>
2020-05-29 07:47:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
 def prime?(number)
-  if number < 0
-    number = number * -1
-  end
+  return false if number < 2 && number > -2
   (2..(number - 1)).each do |value|
     if (number % value) == 0
       return false
