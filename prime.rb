2a65f29f5baccb71181d4e0ef6ce034d1c97e16b
Michael Harris <mwharris7@gmail.com>
2020-05-29 07:47:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2 && number > -2
+  (2..(number - 1)).each do |value|
+    if (number % value) == 0
+      return false
+    end
+  end
+  true
+end
